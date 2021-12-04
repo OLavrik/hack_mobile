158764aeeb5229bc49875dd0055a9dded16943b7
decentNick <Hota6blch@gmail.com>
2021-12-04 07:51:12 +0300

    Some fix

--- collect_data/parse_app_store.py
+++ collect_data/parse_app_store.py
@@ -1,7 +1,29 @@
 import re
 import requests
 import lxml.html
-from utils.process_lang import preprocess_text
+
+import re
+from pymystem3 import Mystem
+mystem = Mystem()
+from string import punctuation
+from nltk.corpus import stopwords
+russian_stopwords = stopwords.words("russian")
+
+def preprocess_text(text):
+    r = re.compile("[а-яА-Я]+")
+    text = text.replace("#", "").replace("  ", " ").replace("\n"," ")
+    tokens = text.lower().split(" ")
+    tokens = [w.lower() for w in filter(r.match, tokens)]
+    tokens = mystem.lemmatize(" ".join(tokens))
+    tokens = [
+        token
+        for token in tokens
+        if token not in russian_stopwords
+           and token != " "
+           and token.strip() not in punctuation
+    ]
+    return " ".join(tokens)
+
 
 def get_class_html(text, class_name="section__description"):
     html = lxml.html.fromstring(text)
